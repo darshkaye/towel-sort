01264098024e751db8ca70313c6c7d29f21ef426
Darya Yermakova <yermakova.dasha@gmail.com>
2020-10-18 22:46:42 +0300

    feat:realize towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) {return [] };
+  return matrix.reduce((prevEl, currEl, index) => prevEl.concat(index % 2 === 0 ? currEl : currEl.reverse()), [])
 }
